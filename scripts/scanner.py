f26e278a48ac87c5bba3d12877672495492adf74
Sergey Kish <sergey.kish@gmail.com>
2009-04-27 10:37:48 +0000

    make Tag easer to test, parse attributes

--- scripts/scanner.py
+++ scripts/scanner.py
@@ -1,14 +1,20 @@
 import re
 
 class Tag():
-    def __init__(self, match, lineno):
-        self.text   = match.group(0)
-        self.start  = match.start()
-        self.end    = match.end()
-        self.lineno = lineno
+    def __init__(self, text, start=0, end=0, lineno=0):
+        self._text   = text
+        self._start  = start
+        self._end    = end
+        self._lineno = lineno
+
+        self._attributes = {}
+        expression = '\s*(?P<attribute>\w+)="(?P<value>.*?)"\s*'
+        pattern = re.compile(expression)
+        for match in pattern.finditer(self._text):
+            self._attributes[match.group('attribute')] = match.group('value')
 
     def body(self):
-        return self.text[1:-1]
+        return self._text[1:-1]
 
     def name(self):
         return self.body()
@@ -16,10 +22,17 @@ class Tag():
     def test(self, attribute, value):
         return '<%s %s="%s">' % (self.body(), attribute, value)
 
+    def attributes(self):
+        return self._attributes.keys()
+
+    def value(self, attribute):
+        return self._attributes[attribute] if self._attributes.has_key(attribute) else ''
+
 def scan(line, lineno):
     expression = '<[\w]+?>'
     pattern = re.compile(expression)
-    result = [Tag(match, lineno) for match in pattern.finditer(line)]
+    result = [Tag(match.group(0), match.start(), match.end(), lineno)
+            for match in pattern.finditer(line)]
     if result == []:
         return None
     return result[-1]
--- scripts/test_scanner.py
+++ scripts/test_scanner.py
@@ -1,7 +1,17 @@
-from scanner import scan
+from scanner import scan, Tag
 
 def test_return_last():
-    assert scan('he<ul><li>one</li><li>two', 0).text, '<li>'
+    assert scan('he<ul><li>one</li><li>two', 0).body() == 'li'
 
 def test_return_open_tags():
-    assert scan('he<ul><li>one</li>two', 0).text, '<li>'
+    assert scan('he<ul><li>one</li>two', 0).body() == 'li'
+
+def test_attributes_list():
+    tag = Tag('<a href="http://google.com" class="varning test">')
+    assert tag.attributes() == ['href', 'class']
+
+def test_attribute_value():
+    tag = Tag('<a href="http://google.com" class="varning test">')
+    assert tag.value('href') == 'http://google.com'
+    assert tag.value('class') == 'varning test'
+    assert tag.value('id') == ''
